1c17f7dce670c4a2a0b8045efa46416ba96dac5d
BY <tanbangyi@gmail.com>
2021-06-09 19:59:43 +0800

    add actor and obstacle conversion scripts

---
+++ scripts/generate_actor_xml.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+import sys
+
+if len(sys.argv) != 3:
+    print("Please input .world file to modify, and number of actors to add")
+    print("python3 generate_actor_xml.py /home/user/gazebo.world 30")
+    exit()
+
+world_file = sys.argv[1]
+num_agents = int(sys.argv[2])
+
+f = open(world_file, "r+")
+contents = f.readlines()
+
+# Find world tag
+actor_insert_line = 0
+for i in range(len(contents)):
+    if contents[i].find('<world') != -1:
+        actor_insert_line = i + 1
+        break
+
+for i in range(num_agents):
+    actor_name = "walking" + str(i)
+    contents.insert(actor_insert_line, '\t\t</actor>\n')
+    contents.insert(actor_insert_line, '\t\t\t</plugin>\n')
+    contents.insert(actor_insert_line, '\t\t\t\t</link>\n')
+    contents.insert(actor_insert_line, '\t\t\t\t\t</model>\n')
+    contents.insert(actor_insert_line, '\t\t\t\t\t\t<model_name>' + actor_name + '_collision_model</model_name>\n')
+    contents.insert(actor_insert_line, '\t\t\t\t\t<model>\n')
+    contents.insert(actor_insert_line, '\t\t\t\t\t<link_name>' + actor_name + '_pose</link_name>\n')
+    contents.insert(actor_insert_line, '\t\t\t\t<link>\n')
+    contents.insert(actor_insert_line, '\t\t\t<plugin name="actor' + str(i) + '_model_plugin" filename="libattach_collision_plugin.so">\n')
+    contents.insert(actor_insert_line, '\t\t\t</plugin>\n')
+    contents.insert(actor_insert_line, '\t\t\t\t<actor_id>' + str(i) + '</actor_id>\n')
+    contents.insert(actor_insert_line, '\t\t\t<plugin name="actor' + str(i) + '_plugin" filename="libactor_plugin.so">\n')
+    contents.insert(actor_insert_line, '\t\t\t</animation>\n')
+    contents.insert(actor_insert_line, '\t\t\t\t<interpolate_x>true</interpolate_x>\n')
+    contents.insert(actor_insert_line, '\t\t\t\t<filename>walk.dae</filename>\n')
+    contents.insert(actor_insert_line, '\t\t\t<animation name="walking">\n')
+    contents.insert(actor_insert_line, '\t\t\t</skin>\n')
+    contents.insert(actor_insert_line, '\t\t\t\t<filename>walk.dae</filename>\n')
+    contents.insert(actor_insert_line, '\t\t\t<skin>\n')
+    # contents.insert(actor_insert_line, '\t\t\t<pose>' + str(i) + ' 0 1.0 0 0 0</pose>\n')
+    contents.insert(actor_insert_line, '\t\t\t<pose>0 0 ' + str(-i-5) + ' 0 0 0</pose>\n')
+    contents.insert(actor_insert_line, '\t\t<actor name="' + actor_name + '">\n')
+    contents.insert(actor_insert_line, '\t\t</model>\n')
+    contents.insert(actor_insert_line, '\t\t\t</link>\n')
+    contents.insert(actor_insert_line, '\t\t\t\t</collision>\n')
+    contents.insert(actor_insert_line, '\t\t\t\t\t</geometry>\n')
+    contents.insert(actor_insert_line, '\t\t\t\t\t\t</box>\n')
+    contents.insert(actor_insert_line, '\t\t\t\t\t\t\t<size>0.35 1.62 0.35</size>\n')
+    contents.insert(actor_insert_line, '\t\t\t\t\t\t<box>\n')
+    contents.insert(actor_insert_line, '\t\t\t\t\t<geometry>\n')
+    contents.insert(actor_insert_line, '\t\t\t\t\t<pose>0 -0.18 0.05 0 -1.5707963267948966 0</pose>\n')
+    contents.insert(actor_insert_line, '\t\t\t\t<collision name="' + actor_name + '_collision_link">\n')
+    contents.insert(actor_insert_line, '\t\t\t<link name="' + actor_name + '_collision_link">\n')
+    contents.insert(actor_insert_line, '\t\t\t<static>true</static>\n')
+    contents.insert(actor_insert_line, '\t\t\t<pose>0 0 ' + str(-i-5) + ' 0 0 0</pose>\n')
+    contents.insert(actor_insert_line, '\t\t<model name="' + actor_name + '_collision_model">\n')
+
+f.seek(0)
+f.writelines(contents)
+f.truncate()
+f.close()
---
+++ scripts/map_to_obstacles.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+import cv2
+import matplotlib.pyplot as plt
+import sys
+
+if len(sys.argv) != 3:
+    print("Please specify map file to obtain obstacles from and specify file to write obstacles, example: ")
+    print("python3 map_to_obstacles.py /home/user/map.pgm /home/user/obstacles.txt")
+    exit()
+
+print("Open map file: " + sys.argv[1])
+print("Write obstacles to file: " + sys.argv[2])
+map_file = sys.argv[1]
+obstacle_file = sys.argv[2]
+
+image = cv2.imread(map_file)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+# Adjust map image to be correct side up to reflect real world. Rotate right, mirror, then flip
+# image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
+image = cv2.flip(image, 0)
+# image = cv2.flip(image, 1)
+
+f = open(obstacle_file, "w+")
+# contents = f.readlines() # Returns array of lines, each index is a line
+contents = []
+
+# # Find scenario tag line, then insert obstacles at the following line
+# obstacle_insert_line = 0 # Insert index is the index that the inserted object will be
+# for i in range(len(contents)):
+#     if contents[i].find('<scenario>') != -1:
+#         obstacle_insert_line = i + 1
+#         break
+
+# # Add new line for clarity in new XML file
+# contents.insert(obstacle_insert_line, '\n')
+
+# Image processing & finding contours
+gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+_, binary = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY_INV)
+element = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
+contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+# Set origin and resolution of obstacles
+x_origin = -12.5
+y_origin = -35
+resolution = 0.05
+for i in range(len(contours)):
+    if(len(contours[i]) < 2):
+        continue
+
+    contour_points = ""
+
+    for j in range(len(contours[i])):
+        x1 = contours[i][j][0][0] * resolution + x_origin
+        y1 = contours[i][j][0][1] * resolution + y_origin
+        contour_points += str(x1) + ',' + str(y1)
+        if j < (len(contours[i]) - 1):
+            contour_points += ':'
+
+        # x2 = contours[i][j+1][0][0] * resolution + x_origin
+        # y2 = contours[i][j+1][0][1] * resolution + y_origin
+        # contents.insert(obstacle_insert_line, '\t<obstacle x1="' + str(x1) + '" y1="' + str(y1) + '" x2="' + str(x2) + '" y2="' + str(y2) + '"/>\n')
+
+    contour_points += '\n'
+    contents.append(contour_points)
+
+# Seek back to start of file and then override with new content
+f.seek(0)
+f.writelines(contents)
+f.truncate()
+f.close()
+
+# # Display image showing contours
+# image = cv2.drawContours(image, contours, -1, (0,255,0), 2)
+# plt.imshow(image)
+# plt.show()
